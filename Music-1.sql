b3ee58aa13b3a11aa86a7e7240970425aa3072ec
Anton Shukshin <shukshin.2023@bk.ru>
2023-04-23 10:25:08 +0300

    Final version

--- Music-1.sql
+++ Music-1.sql
@@ -1,46 +1,53 @@
 CREATE TABLE IF NOT EXISTS Genre (
-	id SERIAL PRIMARY KEY,
+	genre_id INT PRIMARY KEY,
 	name VARCHAR(80) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Executor (
-	id SERIAL PRIMARY KEY,
+	executor_id INT PRIMARY KEY,
 	name VARCHAR(80) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS GenreExecutor (
-	genre_id INTEGER REFERENCES Genre(id),
-	executor_id INTEGER REFERENCES Executor(id),
-	CONSTRAINT pk PRIMARY KEY (genre_id, executor_id)
+	genre_id INT,
+	executor_id INT,
+	PRIMARY KEY (genre_id, executor_id),
+	FOREIGN KEY (genre_id) REFERENCES Genre(genre_id),
+	FOREIGN KEY (executor_id) REFERENCES Executor(executor_id)
 );
 
-CREATE TABLE IF NOT EXISTS Album (
-	id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Albummums (
+	album_id INT PRIMARY KEY,
 	name VARCHAR(80) UNIQUE NOT NULL,
-	years VARCHAR(4) NOT NULL
+	years INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ExecutorAlbum (
-	album_id INTEGER REFERENCES Album(id),
-	executor_id INTEGER REFERENCES Executor(id),
-	CONSTRAINT pk PRIMARY KEY (album_id, executor_id)
+	album_id INT,
+	executor_id INT,
+	PRIMARY KEY (album_id, executor_id),
+	FOREIGN KEY (album_id) REFERENCES Album(album_id),
+	FOREIGN KEY (executor_id) REFERENCES Executor(executor_id)
 );
 
 CREATE TABLE IF NOT EXISTS Track (
-	id SERIAL NOT NULL,
+	track_id int PRIMARY KEY,
 	name VARCHAR(80) NOT NULL,
-	length VARCHAR(10) NOT NULL,
-	album VARCHAR(80) PRIMARY KEY REFERENCES Album(name)
+	length INT NOT NULL,
+	album_id INT NOT NULL,
+	FOREIGN KEY (album_id) REFERENCES Album(album_id)
 );
 
 CREATE TABLE IF NOT EXISTS Сollection (
-	id SERIAL PRIMARY KEY,
+	collection_id INT PRIMARY KEY,
 	name VARCHAR(80) UNIQUE NOT NULL,
-	years VARCHAR(4) NOT NULL
+	years INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS TrackCollection (
-	track_id INTEGER REFERENCES Track(id),
-	collection_id INTEGER REFERENCES Collection(id),
-	CONSTRAINT pk PRIMARY KEY (track_id, collection_id)
+	track_id INT,
+	collection_id INT,
+	PRIMARY KEY (track_id, collection_id),
+	FOREIGN KEY (track_id) REFERENCES Track(track_id),
+	FOREIGN KEY (collection_id) REFERENCES Collection(collection_id)
 );
